6e987b6d24dcdc1943d5a89f5e60c33efa59ddb3
Freyr Danielsson <freyrdanielsson@gmail.com>
2017-11-20 16:44:39 +0000

    origin eftirá?

--- script-compiled.js
+++ script-compiled.js
@@ -8,13 +8,46 @@ var Player = function () {
   function Player() {
     _classCallCheck(this, Player);
 
-    this.ex = 'player test';
+    //lykill fyrir geymt info í localstorage?
+    this.keyName = 'info';
+
+    this.container = document.querySelector('.container');
+    this.heading = this.container.querySelector('.heading');
+    //finna takka
+    this.prev = this.container.querySelector('.player .prev');
+    this.play = this.container.querySelector('.player .play');
+    this.sound = this.container.querySelector('.player .sound');
+    this.fullscreen = this.container.querySelector('.player .fullscreen');
+    this.next = this.container.querySelector('.player .next');
   }
 
   _createClass(Player, [{
-    key: 'test',
-    value: function test() {
-      console.log(this.ex);
+    key: 'load',
+    value: function load() {
+      var saved = window.localStorage.getItem(this.keyName);
+
+      if (saved) {
+        var parse = JSON.parse(saved);
+        //ath betur hvernig video info er, video er sótt
+        this.create(parse.title, parse.video);
+      }
+    }
+
+    //hugmynd
+
+  }, {
+    key: 'create',
+    value: function create(title, video) {
+      //setja <h1> sem titil myndbands
+      this.heading.appendChild(document.createTextNode(title));
+
+      //Event listener fyrir takka, þarf að búa til þessi föll
+      //geta líka verið nafnlaus föll fyrir einfaldari takkana
+      this.prev.addEventListener('click', this.prevfun.bind(this));
+      this.play.addEventListener('click', this.playfun.bind(this));
+      this.sound.addEventListener('click', this.soundfun.bind(this));
+      this.fullscreen.addEventListener('click', this.fullscreenfun.bind(this));
+      this.next.addEventListener('click', this.nextfun.bind(this));
     }
   }]);
 
@@ -23,7 +56,7 @@ var Player = function () {
 
 document.addEventListener('DOMContentLoaded', function () {
   var player = new Player();
-  player.test();
+  player.load();
 });
 'use strict';
 
